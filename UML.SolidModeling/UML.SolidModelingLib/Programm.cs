2321b428ae9ad474495d009c4c7b7aca594ac177
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-01-29 17:28:46 -0500

    Updatin code example

--- UML.SolidModeling/UML.SolidModelingLib/Programm.cs
+++ UML.SolidModeling/UML.SolidModelingLib/Programm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UML.SolidModelingLib
 {
@@ -6,29 +7,18 @@ namespace UML.SolidModelingLib
     {
         static void Main()
         {
-            var client = new Customer();
-            client.CustomerType = CustomersType.Gold_Partner;
-            var discount = client.getDiscount(1500000);
+            var clientList = new List<Customer>();
+            double sales = 1500000;
 
-            Console.WriteLine("Discount achieved for a client was: $ {0}", discount.ToString("N2"));
-            Console.ReadLine();
+            clientList.Add(new GoldCustomer());
+            clientList.Add(new SilverCustomer());
+            clientList.Add(new RegisteredCustomer());
 
-            var goldClient = new GoldCustomer();
-            var goldDiscount = goldClient.getDiscount(1500000);
-
-            Console.WriteLine("Discount achieved for gold was: $ {0}", goldDiscount.ToString("N2"));
-            Console.ReadLine();
-
-            var silverClient = new SilverCustomer();
-            var silverDiscount = silverClient.getDiscount(1500000);
-
-            Console.WriteLine("Discount achieved for silver was: $ {0}", silverDiscount.ToString("N2"));
-            Console.ReadLine();
-
-            var registeredClient = new RegisteredCustomer();
-            var registeredDiscount = registeredClient.getDiscount(1500000);
-
-            Console.WriteLine("Discount achieved for registered was: $ {0}", registeredDiscount.ToString("N2"));
+            foreach (var item in clientList)
+            {
+                var discount = item.getDiscount(sales);
+                Console.WriteLine("Discount achieved for {0} was: $ {1}", item.GetType().ToString(), discount.ToString("N2"));
+            }
             Console.ReadLine();
         }
     }
