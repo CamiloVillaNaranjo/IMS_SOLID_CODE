e099546586d4dbfc2514c3fcf89bba197ba0c2fc
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-01-29 17:35:27 -0500

    Updating code example

--- UML.SolidModeling/UML.SolidModelingLib/GeneratedCode/FileLogger.cs
+++ UML.SolidModeling/UML.SolidModelingLib/GeneratedCode/FileLogger.cs
@@ -4,10 +4,7 @@
 //     Changes to this file will be lost if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 public class FileLogger
 {
